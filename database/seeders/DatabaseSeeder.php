4bc83afd2860c8f42342d24453a3d08e7d868f04
BeRobi <screengsm@gmail.com>
2024-02-19 17:40:10 +0100

    turatipus mfc ujra

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\Turavezeto::factory(10)->create();
-         \App\Models\TuraTipus::factory(10)->create();
+        // \App\Models\TuraTipus::factory(10)->create();
          \App\Models\Tura::factory(10)->create(); 
           \App\Models\Turavezeto::factory(10)->create();
 
